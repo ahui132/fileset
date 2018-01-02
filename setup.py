17b66674731ad481b103aad014fe2897c1d357a8
hilojack <a132811@gmail.com>
2018-01-02 22:38:42 +0800

    0.4

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 setup(
     name = "fileset",
-    version = "0.0.3",
+    version = "0.0.4",
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     scripts = ['fileset'],
 
-    install_requires = [
-        ],
+    install_requires = [],
 
     # metadata for upload to PyPI
-    author = "hilojack",
+    author = "ahui132",
     author_email = "a132811@gmail.com",
     description = "This is a cli tool used to calculate line set of two files",
     license = "GPL",
     keywords = "fileset",
-    url = "http://github.com/hilojack/fileset", 
+    url = "http://github.com/ahui132/fileset", 
 
     # could also include long_description, download_url, classifiers, etc.
 )
